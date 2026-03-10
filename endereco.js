21d1641e6122cb705b46d8358e59126cb1c3fc60
mila254 <camila.serna@escola.pr.gov.br>
2026-03-10 11:44:13 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,8 @@
+console.log("endereço completo");
+let rua = "Rua das flores";
+let numero = 489;
+let cidade = "Curitiba";
+
+let endereco = rua + " , " + numero + " - " + cidade;
+//console.log(endereco);
+console.log("O endereço correto é: " + endereco);
