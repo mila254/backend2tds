6b00fc48c3503fdcdf338babd44b2df83d8a67f3
mila254 <camila.serna@escola.pr.gov.br>
2026-03-17 14:45:56 +0000

    versao2

--- hora.js
+++ hora.js
@@ -1,3 +1,10 @@
 //valores iniciais do dia
 let valorPorhora = 50;
 let horasTrabalhadas = 8;
+
+console.log("---Resumo do Expediente---");
+console.log(`Horas iniciais:${horasTrabalhadas}h.`);
+
+//git add hora.js
+//git commit -m "versao2"
+//git push
