25a5f383494c48959e438ae48b6eb2bc376ec96c
mila254 <camila.serna@escola.pr.gov.br>
2026-05-18 13:54:32 +0000

    versao2

--- desafio.js
+++ desafio.js
@@ -1,9 +1,15 @@
 //5 variáveis para jogador com interpolação
-let nomeJogador = "Rafael"
-const nickJogo = "Arthur"
+let nomeJogador = "Rafael";
+const nickJogo = "Arthur";
 
-const nomeJogo = "Ordem paranormal"
-let objetivo = "Ajudar as pessoas"
-let recompenca = "Derrotar os espíritos"
+const nomeJogo = "Ordem paranormal";
+let objetivo = "Ajudar as pessoas";
+let recompenca = "Derrotar os espíritos";
 
-console.log(`${nomeJogador} jogou ${nomeJogo} com o personagem ${nickJogo} seu objetivo era ${objetivo} e prentedia ${recompenca}`)
+console.log(`${nomeJogador} jogou ${nomeJogo} com o personagem ${nickJogo} seu objetivo era ${objetivo} e prentedia ${recompenca}`);
+
+let presenca = 1;
+let dado = 20;
+let resultado = dado - presenca; 
+
+console.log(`O resultado é ${resultado}.`)
